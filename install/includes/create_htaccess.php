f9286e6c2f1eadbeb01a6f2aeefe61aa49311643
Scott Gearhart <sgearhart2@gmail.com>
2020-07-27 21:01:45 -0400

    Add composer and use it's autoload.

--- install/includes/create_htaccess.php
+++ install/includes/create_htaccess.php
@@ -4,7 +4,7 @@ $appRoot = realpath(__DIR__ . "/../../") ;
 $htaccessString = "";
 
 $htaccessString .= "php_value include_path \".;" . $appRoot . "/\"" . PHP_EOL;
-$htaccessString .= "php_value auto_prepend_file \"" . $appRoot . "/autoload.php\"" . PHP_EOL;
+$htaccessString .= "php_value auto_prepend_file \"" . $appRoot . "/vendor/autoload.php\"" . PHP_EOL;
 
 
 file_put_contents('../../.htaccess', $htaccessString);
